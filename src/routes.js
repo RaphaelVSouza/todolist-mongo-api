fc8929f2b431f551a004e576d6904ebc9929424c
Raphael Vieira <raphael5254@outlook.com>
2020-11-17 22:58:12 -0300

    Added express-brute-redis and new routes for user-management

--- src/routes.js
+++ src/routes.js
@@ -1,41 +1,39 @@
 const { Router } = require('express');
 const ExpressBrute = require('express-brute');
+const RedisStore = require('express-brute-redis');
 
+const redisConfig = require('./config/redisConfig');
 const routes = new Router();
 const authMiddleware = require('./app/middlewares/auth');
 
-if(process.env.NODE_ENV === 'production') {
-    console.warn("Don't use local storage in express-brute on production!")
-} 
+const store = new RedisStore(redisConfig);
     
-const store = new ExpressBrute.MemoryStore(); 
 const bruteforce = new ExpressBrute(store);
 
 const UserController = require('./app/controllers/UserController');
 const SessionController = require('./app/controllers/SessionController');
 const PasswordController = require('./app/controllers/PasswordController');
 const ProjectController = require('./app/controllers/ProjectController');
-
-
-routes.get('/', (req, res) => {
-    res.json({message: 'rodou'});
-})
+const UserMailController = require('./app/controllers/UserMailController');
 
 routes.post('/user-management/register', UserController.store);
 
 
-routes.post('/user-management/login', bruteforce.prevent, SessionController.store);
+routes.post('/user-management/login', bruteforce.prevent,  SessionController.store);
 
+routes.get('/user-management/verify_email/:verifyToken', UserMailController.verifyEmail);
 
-routes.post('/user-management/forgot_password', PasswordController.store)
-routes.post('/user-management/reset_password', PasswordController.update)
+routes.post('/user-management/forgot_password', PasswordController.store);
+routes.post('/user-management/reset_password/:resetToken', PasswordController.update);
 
 routes.use(authMiddleware);
 
-routes.post('/projects/new', ProjectController.store)
-routes.get('/projects', ProjectController.index)
-routes.get('/projects/:projectId', ProjectController.show)
-routes.put('/projects/:projectId/edit', ProjectController.update)
-routes.delete('/projects/:projectId/delete', ProjectController.delete)
+routes.put('/user-management/edit', SessionController.update);
+
+routes.post('/projects/new', ProjectController.store);
+routes.get('/projects', ProjectController.index);
+routes.get('/projects/:projectId', ProjectController.show);
+routes.put('/projects/:projectId/edit', ProjectController.update);
+routes.delete('/projects/:projectId/delete', ProjectController.delete);
 
 module.exports = routes;
