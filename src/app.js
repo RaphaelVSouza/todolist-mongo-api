d069ffd1c8a907f672aed13af15caa0eb78393d4
Raphael Vieira <raphael5254@outlook.com>
2020-11-17 22:56:10 -0300

    Added error handling middleware on app.js

--- src/app.js
+++ src/app.js
@@ -23,6 +23,10 @@ class App {
         this.server.use(limiter);
         this.server.use(express.json());
         this.server.use(express.urlencoded({extended:false}));
+        this.server.use((err, req, res, next) => {
+            console.error(err);
+            res.status(500).send('Something broke!');
+          });
     }
     routes() {
         this.server.use(routes);
