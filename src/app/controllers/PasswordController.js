cbe512913f6b53f3627f7d147cb04d4c15970d51
Raphael Vieira <raphael5254@outlook.com>
2020-11-19 18:28:45 -0300

    Moved password change mail to queue

--- src/app/controllers/PasswordController.js
+++ src/app/controllers/PasswordController.js
@@ -1,8 +1,10 @@
 const User = require('../schemas/Users');
 const crypto = require('crypto');
-const Mail = require('../../lib/mail');
 
-const apiUrl = process.env.BACK_URL;
+const Queue = require('../../lib/queue');
+const ChangePassMail = require('../jobs/ChangePassMail');
+
+const apiUrl = process.env.SERVER_URL;
 
 class PasswordController {
     async store(req, res) {
@@ -11,28 +13,28 @@ class PasswordController {
         const user = await User.findOne({ email });
 
         if (!user) {
-            return res.status(400).json({error: 'User not found'});
+            return res.status(400).send({error: 'User not found'});
         }
-        const token = crypto.randomBytes(20).toString('hex');
+
+        if(!user.verifiedEmail) {
+            return res.status(400).send({error: 'Email must be verified first'})
+        }
+        
+        const resetToken = crypto.randomBytes(20).toString('hex');
 
         const now = new Date();
         now.setHours(now.getHours() + 1);
 
         await User.findByIdAndUpdate(user.id, {
             '$set': {
-                passwordResetToken: token,
+                passwordResetToken: resetToken,
                 passwordResetExpires: now,
             }, 
         }, { useFindAndModify: false })
 
-        await Mail.sendMail({
-            to: email,
-            from: 'random@company.com.br',
-            subject: 'Change password',
-            template: 'forgot_password',
-            context: { token, apiUrl } 
-          });
-    
+       
+        await Queue.add(ChangePassMail.key, { email, apiUrl, resetToken });
+
         return res.json({message: 'Email successfully sent'})
 
     }
