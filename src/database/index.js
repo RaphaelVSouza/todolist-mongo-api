16cc46d7462291b407881c96532469d4d2758b0a
Raphael Vieira <raphael5254@outlook.com>
2020-11-18 21:35:48 -0300

    fix: changed database name

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/loginjwt', {
+mongoose.connect('mongodb://localhost/todolist', {
      useNewUrlParser: true,
       useUnifiedTopology: true
     }
