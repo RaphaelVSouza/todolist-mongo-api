aaf55a0c9eea70d7128b1741de4f7cecfd879aee
Raphael Vieira <raphael5254@outlook.com>
2021-01-08 19:37:10 -0300

    connect, isConnect and close methods to mongo

--- src/database/mongo.js
+++ src/database/mongo.js
@@ -1,23 +1,47 @@
-import mongoose from 'mongoose';
+import Mongoose from 'mongoose';
 
-class MongoDB {
+class Mongodb {
   constructor() {
-    this.mongoConnect();
+    this.connect();
+    this._status = {
+      0: 'Disconnected',
+      1: 'Connected',
+      2: 'Connecting',
+      3: 'Disconnecting',
+    };
   }
 
-  async mongoConnect() {
-    const dbName = process.env.MONGODB_NAME;
-    const connectionUrl = `mongodb://localhost/${dbName}`;
+  connect() {
+    const connectionUrl = process.env.MONGO_URL;
+    const mongooseOptions = {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: true,
+    };
 
-    await mongoose
-      .connect(connectionUrl, {
-        useCreateIndex: true,
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: true,
-      })
-      .then(() => console.log('Connected in mongodb'));
+    Mongoose.connect(connectionUrl, mongooseOptions).catch((reject) => console.log(reject));
+
+    const { connection } = Mongoose;
+
+    this._connection = connection;
+  }
+
+  async isConnected() {
+    const state = this._status[this._connection.readyState];
+
+    if (state === 'Connected') return state;
+
+    if (state !== 'Connecting') return state;
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    return this._status[this._connection.readyState];
+  }
+
+  async close() {
+    await this._connection.close(true);
   }
 }
 
-export default new MongoDB();
+export default new Mongodb();
