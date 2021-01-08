1414e7242e88d6e33d128d8d85510fd70fb5ac49
Raphael Vieira <raphael5254@outlook.com>
2021-01-08 19:24:13 -0300

    linted

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,7 +1,5 @@
 import bcrypt from 'bcryptjs';
-import mongoose from 'mongoose';
 import User from '../schemas/Users.js';
-import UserMail from '../schemas/UserMails';
 
 class SessionController {
   async store(req, res) {
@@ -9,20 +7,21 @@ class SessionController {
 
     const user = await User.findOne({ email }).select('id name +password isVerified');
 
-    if (!user) return res.status(404).send({ error: 'User not found' });
+    if (!user) return res.status(404).send({ error: 'User or password invalid' });
 
     if (!user.isVerified) return res.status(401).send({ error: 'Need to verify email first' });
 
-    if (!(await bcrypt.compare(password, user.password))) return res.status(401).send({ error: 'Invalid password' });
+    if (!(await bcrypt.compare(password, user.password))) return res.status(401).send({ error: 'User or password invalid' });
 
     const { id, name } = user;
 
     return res.json({
-      id, name, email, accessToken: user.generateAccessToken({ id: user.id }),
+      id,
+      name,
+      email,
+      accessToken: user.generateAccessToken({ id: user.id }),
     });
   }
-
-  async update(req, res) {}
 }
 
 export default new SessionController();
