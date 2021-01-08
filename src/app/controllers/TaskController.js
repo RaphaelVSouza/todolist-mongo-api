4c91b9b2914dee67bbc53882df438626b45f87e6
Raphael Vieira <raphael5254@outlook.com>
2021-01-08 19:24:57 -0300

    added delete method

--- src/app/controllers/TaskController.js
+++ src/app/controllers/TaskController.js
@@ -1,4 +1,5 @@
-import Task from '../schemas/Tasks.js';
+import Task from '../schemas/Tasks';
+import Project from '../schemas/Projects';
 
 class TaskController {
   async update(req, res) {
@@ -6,11 +7,41 @@ class TaskController {
 
     if (!userId) return res.status(401).send({ error: 'You must be logged in to see your projects' });
 
-    const { taskId } = req.body;
+    const { projectId, taskId } = req.params;
+
+    if (!projectId) return res.status(400).send({ error: 'projectId must be suplied' });
 
     if (!taskId) return res.status(400).send({ error: 'TaskId must be suplied' });
 
     const task = await Task.findById(taskId);
+
+    if (!task) return res.status(404).send({ error: 'Task not found' });
+
+    task.completed ? (task.completed = false) : (task.completed = true);
+
+    task.save();
+
+    return res.json(task);
+  }
+
+  async delete(req, res) {
+    const { userId } = req.user;
+
+    if (!userId) return res.status(401).send({ error: 'You must be logged in to see your projects' });
+
+    const { projectId, taskId } = req.params;
+
+    if (!projectId) return res.status(400).send({ error: 'projectId must be suplied' });
+
+    const project = await Project.findById(projectId);
+
+    if (!project) return res.status(404).send({ error: 'Project not found' });
+
+    if (!taskId) return res.status(400).send({ error: 'TaskId must be suplied' });
+
+    const task = await Task.findById(taskId);
+
+    if (!task) return res.status(404).send({ error: 'Task not found' });
 
     task.completed ? (task.completed = false) : (task.completed = true);
 
